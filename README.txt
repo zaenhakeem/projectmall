534194afd32b8487c690266c9412c3fe849d45c3
zaenhakeem <74319261+zaenhakeem@users.noreply.github.com>
2022-04-07 16:23:28 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+To run 
+
+1. create a virtual environment
+2. on the terminal run "pip install -r requirements.txt"
+3. run "python manage.py runserver"
---
+++ store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Featuredproduct)
+admin.site.register(ShippingAddress)
+admin.site.register(Contact)
